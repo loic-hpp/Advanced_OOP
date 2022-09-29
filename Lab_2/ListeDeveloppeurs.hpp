f1f888770997e71a22b547564799062df73e4dd9
Rodri Merino <rodrimerino201@gmail.com>
2022-09-29 16:42:25 -0400

    Faire base initiale de la classe.

--- Lab_2/ListeDeveloppeurs.hpp
+++ Lab_2/ListeDeveloppeurs.hpp
@@ -5,8 +5,20 @@
 class ListeDeveloppeurs
 {
 	//TODO: En faire une classe qui suit les principes OO.
-	//TODO: On veut pouvoir ajouter et enlever un Developpeur* de la liste, avec réallocation dynamique tel que faite pour ListeJeux.
-	//NOTE: Le code sera principalement copié de certaines fonctions écrites pour la partie 1, mais mises dans une classe.
+public:
+	ListeDeveloppeurs();
+
+private:
 	std::size_t nElements, capacite;
 	Developpeur** elements;
+};
+
+ListeDeveloppeurs::~ListeDeveloppeurs()
+{
+
+}
+
+//TODO: On veut pouvoir ajouter et enlever un Developpeur* de la liste, avec réallocation dynamique tel que faite pour ListeJeux.
+
+//NOTE: Le code sera principalement copié de certaines fonctions écrites pour la partie 1, mais mises dans une classe.
 };
