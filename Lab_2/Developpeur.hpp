e64562095dd9930e8d5320769fa43de1c698de26
Rodri Merino <rodrimerino201@gmail.com>
2022-09-29 16:31:49 -0400

    Faire commit initial de la classe Developpeur.

--- Lab_2/Developpeur.hpp
+++ Lab_2/Developpeur.hpp
@@ -1,14 +1,53 @@
 ﻿#pragma once
 #include <string>
 #include "ListeJeux.hpp"
+#include <string>
+
+using namespace std;
 
 class Developpeur
 {
 	//TODO: En faire une classe qui suit les principes OO.
-	
-	//TODO: La destruction d'un Developpeur doit s'assurer que la désallocation de ListeJeux est faite.
-	
-	//TODO: Les méthodes à faire...
+public:
+	Developpeur(ListeJeux listeJeux);
+	string obtenirNom() const;
+	int obtenirNombreJeuxDeveloppes(ListeJeux listeJeux);
+	ListeJeux mettreAJourListeJeux(ListeJeux listeJeuxComplete);
 
-	std::pair<std::string, ListeJeux> paireNomJeux_;
+private:
+	string name_;
+	string paireNomJeux_;
+	int nombreJeuxDeveloppes;
+};
+
+//TODO: La destruction d'un Developpeur doit s'assurer que la désallocation de ListeJeux est faite.
+~Developpeur() {};
+
+//TODO: Les méthodes à faire...
+string Developpeur::obtenirNom() const
+{
+	//TODO	
+}
+
+int Developpeur::obtenirNombreJeuxDeveloppes(ListeJeux listeJeux)
+{
+	//TODO
+}
+
+ListeJeux mettreAJourListeJeux(ListeJeux listeJeuxComplete) 
+{
+	//TODO
+}
+
+void afficherListeJeuxDeveloppes(ListeJeux listeJeuxDeveloppes)
+{
+	int i = 0;
+	for (auto i : listeJeuxDeveloppes) 
+	{
+		//TODO
+	}
+
+}
+
+std::pair<std::string, ListeJeux> paireNomJeux_;
 };
