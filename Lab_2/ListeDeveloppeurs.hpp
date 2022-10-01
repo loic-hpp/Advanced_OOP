12225203275fa45c580b7e9069a1c35592bbddf6
Rodri Merino <rodrimerino201@gmail.com>
2022-10-01 15:28:17 -0400

    Renommer variables des methodes ajouter, retirer et changer de taille d'une liste de Developpeur.

--- Lab_2/ListeDeveloppeurs.hpp
+++ Lab_2/ListeDeveloppeurs.hpp
@@ -16,12 +16,11 @@ private:
 	Developpeur** elements;
 };
 
-// TODO: On veut pouvoir ajouter et enlever un Developpeur* de la liste, 
-// avec réallocation dynamique tel que faite pour ListeJeux.
-
 ListeDeveloppeurs::~ListeDeveloppeurs()
 {
 	// Liberer
+	// TODO: On veut pouvoir ajouter et enlever un Developpeur* de la liste, 
+// avec réallocation dynamique tel que faite pour ListeJeux.
 }
 
 void afficher(ListeJeux listeJeux)
@@ -29,44 +28,44 @@ void afficher(ListeJeux listeJeux)
 	afficherListeJeuxDeveloppes(listeJeux);
 }
 
-void ajouterDeveloppeur(ListeJeux& listeJeux, Developpeur* developpeur)
+void ajouterDeveloppeur(ListeDeveloppeurs& listeDeveloppeurs, Developpeur* developpeur)
 {
 	//TODO: à adapter 
-	size_t index = listeJeux.nElements;
-	if (listeJeux.nElements == listeJeux.capacite) {
-		if (listeJeux.capacite == 0)
+	size_t index = listeDeveloppeurs.nElements;
+	if (listeDeveloppeurs.nElements == listeDeveloppeurs.capacite) {
+		if (listeDeveloppeurs.capacite == 0)
 			index = 0;
-		changerTailleListeJeux(listeJeux);
+		changerTailleListeJeux(listeDeveloppeurs);
 	}
-	listeJeux.elements[index] = developpeur;
-	listeJeux.nElements++;
+	listeDeveloppeurs.elements[index] = developpeur;
+	listeDeveloppeurs.nElements++;
 }
 
-void changerTailleListeJeux(ListeJeux& listeJeux) {
-	if (listeJeux.capacite == 0)
-		listeJeux.capacite = 2;
+void changerTailleListeDeveloppeurs(ListeDeveloppeurs& listeDeveloppeurs) {
+	if (listeDeveloppeurs.capacite == 0)
+		listeDeveloppeurs.capacite = 2;
 	else
-		listeJeux.capacite *= 2;
-	Developpeur** nouvelleListe = new Developpeur * [listeJeux.capacite];
+		listeDeveloppeurs.capacite *= 2;
+	Developpeur** nouvelleListe = new Developpeur * [listeDeveloppeurs.capacite];
 	int i = 0;
-	for (Developpeur* developpeur : gsl::span(listeJeux.elements, listeJeux.nElements)) {
+	for (Developpeur* developpeur : gsl::span(listeDeveloppeurs.elements, listeDeveloppeurs.nElements)) {
 		nouvelleListe[i] = developpeur;
 		i++;
 	}
-	delete[] listeJeux.elements;
-	listeJeux.elements = nullptr;
-	listeJeux.elements = nouvelleListe;
+	delete[] listeDeveloppeurs.elements;
+	listeDeveloppeurs.elements = nullptr;
+	listeDeveloppeurs.elements = nouvelleListe;
 }
 
-void retirerDeveloppeur(ListeJeux& listeJeux, Developpeur* developpeur)
+void retirerDeveloppeur(ListeDeveloppeurs& listeDeveloppeurs, Developpeur* developpeurParametre)
 {
 	int i = 0;
-	for (Jeu*& jeu : gsl::span(listeJeux.elements, listeJeux.nElements)) {
-		if (developpeur == jeu)
-			listeJeux.elements[i] = listeJeux.elements[listeJeux.nElements - 1];
+	for (Developpeur*& developpeur : gsl::span(listeDeveloppeurs.elements, listeDeveloppeurs.nElements)) {
+		if (developpeurParametre == developpeur)
+			listeDeveloppeurs.elements[i] = listeDeveloppeurs.elements[listeDeveloppeurs.nElements - 1];
 		i++;
 	}
-	listeJeux.elements[listeJeux.nElements - 1] = nullptr;
-	listeJeux.nElements--;
+	listeDeveloppeurs.elements[listeDeveloppeurs.nElements - 1] = nullptr;
+	listeDeveloppeurs.nElements--;
 }
 //NOTE: Le code sera principalement copié de certaines fonctions écrites pour la partie 1, mais mises dans une classe.
