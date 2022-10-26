6ee33be8320fd6ab53ed49390c2cb49f9aed926a
Rodri Merino <rodrimerino201@gmail.com>
2022-10-26 09:08:07 -0400

    Creation du projet TD4

---
+++ Lab_4/lectureBinaire.cpp
@@ -0,0 +1,38 @@
+﻿#include "lectureBinaire.hpp"
+#include <exception>
+#include <cassert>
+using namespace std;
+
+namespace {
+
+template <typename T>
+T lireType(istream& fichier)
+{
+	T valeur{};
+	fichier.read(reinterpret_cast<char*>(&valeur), sizeof(valeur));
+	return valeur;
+}
+
+}
+
+#define erreurFataleAssert(message) assert(false&&(message)),terminate()
+static const uint8_t enteteTailleVariableDeBase = 0xA0;
+size_t lireUintTailleVariable(istream& fichier)
+{
+	uint8_t entete = lireType<uint8_t>(fichier);
+	switch (entete) {
+	case enteteTailleVariableDeBase+0: return lireType<uint8_t>(fichier);
+	case enteteTailleVariableDeBase+1: return lireType<uint16_t>(fichier);
+	case enteteTailleVariableDeBase+2: return lireType<uint32_t>(fichier);
+	default:
+		erreurFataleAssert("Tentative de lire un entier de taille variable alors que le fichier contient autre chose à cet emplacement.");
+	}
+}
+
+string lireString(istream& fichier)
+{
+	string texte;
+	texte.resize(lireUintTailleVariable(fichier));
+	fichier.read((char*)&texte[0], streamsize(sizeof(texte[0])) * texte.length());
+	return texte;
+}
