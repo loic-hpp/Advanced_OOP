21f86e168fafea86fbc3ba8eff67dfe475d4a62f
Rodri Merino <rodrimerino201@gmail.com>
2022-11-06 12:25:23 -0500

    Finir implementation de Personnage.

--- Lab_4/Personnage.cpp
+++ Lab_4/Personnage.cpp
@@ -1,10 +1,30 @@
 #include "Personnage.hpp"
 
-string Personnage::afficher(string texte) const 
+Personnage::Personnage(string& nom, string& parution)
 {
-
+	nom_ = nom;
+	parution_ = parution;
 }
-Couleur Personnage::changerCouleur(Couleur couleur) const 
-{
 
+void Personnage::afficher(std::ostream& os) const
+{
+	os <<
+		"Nom : " << obtenirNom() << "\n" <<
+		"Parution : " << obtenirParution() << "\n";
+	// Loic fait CODE_COULEUR_FIN?
+}
+
+void Personnage::changerCouleur(std::ostream& os, const int couleurAffichage) const
+{
+	changerCouleur(os, 23);
+}
+
+string Personnage::obtenirNom() const
+{
+	return nom_;
+}
+
+string Personnage::obtenirParution() const
+{
+	return parution_;
 }
