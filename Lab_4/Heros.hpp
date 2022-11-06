2729f1867f1dfd6df0bbdc35112226d232ad871b
Rodri Merino <rodrimerino201@gmail.com>
2022-11-06 13:07:09 -0500

    Nettoyer definitions de Personnage et Heros.

--- Lab_4/Heros.hpp
+++ Lab_4/Heros.hpp
@@ -1,15 +1,25 @@
 #pragma once
 #include <string>
 #include "Personnage.hpp"
+#include <vector>
 
 using namespace std;
 
-// TODO
-class Heros: public Personnage
+class Heros: virtual public Personnage
 {
 public:
-	Heros();
+	Heros(string& nom, string& parution, string& nomEnnemi);
+	void afficher(std::ostream& os) const override;
+	void changerCouleur(std::ostream& os, const int couleurAffichage) const override;
+	string obtenirNom() const override;
+	string obtenirParution() const override;
 	
+	virtual void obtenirNomEnnemi() const;
+	virtual void afficherAllies(ostream& os, int& couleur) const;
+	virtual void mettreAllies(const vector<string> nomAllies);
+	
+	virtual ~Heros() = default;
 private:
-	string nom;
+	vector<string> nomAllies_;
+	string nomEnnemi_;
 };
--- Lab_4/Personnage.hpp
+++ Lab_4/Personnage.hpp
@@ -5,14 +5,14 @@
 class Personnage : public Affichable
 {
 public:
-	Personnage() = default;
 	Personnage(string& nom, string& parution);
 	void afficher(std::ostream& os) const override;
 	void changerCouleur(std::ostream& os, const int couleurAffichage) const override;
-	virtual ~Personnage() = default;
+	
 	virtual string obtenirNom() const;
 	virtual string obtenirParution() const;
 
+	virtual ~Personnage() = default;
 private:
 	string nom_;
 	string parution_;
