fdee7997d5aa53ab1aeb6bee8d24f4fb1eac7edd
Rodri Merino <rodrimerino201@gmail.com>
2022-11-06 13:17:45 -0500

    Finir definition de Villain.

--- Lab_4/Villain.hpp
+++ Lab_4/Villain.hpp
@@ -1,3 +1,23 @@
 #pragma once
+#include <string>
+#include "Personnage.hpp"
+#include <vector>
 
-// TODO
+using namespace std;
+
+class Villain : virtual public Personnage
+{
+public:
+	Villain(string& nom, string& parution, string& objectif);
+	void afficher(std::ostream& os) const override;
+	void changerCouleur(std::ostream& os, const int couleurAffichage) const override;
+	string obtenirNom() const override;
+	string obtenirParution() const override;
+
+	virtual void obtenirObjectif() const;
+	virtual void afficherObjectif(ostream& os, int& couleur) const;
+
+	virtual ~Villain() = default;
+private:
+	string objectif_;
+};
