41d271f01da1fd75898660e4637f414a99a4d81a
Rodri Merino <rodrimerino201@gmail.com>
2022-11-07 16:07:05 -0500

    Terminer implementation de base de la classe Heros.

--- Lab_4/Heros.cpp
+++ Lab_4/Heros.cpp
@@ -1,12 +1,47 @@
-#include "Heros.hpp"
+//	Implementation de la classe Heros.
+//	Fichier:  Liste.hpp
+//	Auteurs: Rodrigo A. Merino Martel et Loïc Nguemegne Temena
+//	Date	8 novembre 2022
+//	Créé le 6 novembre 2022
 
-Heros();
-Heros(string& nom, string& parution, vector<string> nomAllies, string& nomEnnemi);
-void afficher(std::ostream& os) const override;
-void changerCouleur(std::ostream& os, const int couleurAffichage) const override;
-virtual ~Heros() = default;
-string obtenirNom() const override;
-string obtenirParution() const override;
-virtual void obtenirNomEnnemi() const;
-virtual void afficherAllies(ostream& os, int& couleur) const;
-virtual void poserAllies(const vector<string> nomAllies);
+#include "Heros.hpp"
+#include "cppitertools/range.hpp"
+using namespace iter;
+
+Heros::Heros(string& nom, string& parution, string& nomEnnemi) :
+	Personnage(nom, parution),
+	nomEnnemi_(nomEnnemi)
+{
+}
+
+void Heros::afficher(std::ostream& os) const
+{
+	changerCouleur(os, 94);
+	afficherAllies;
+}
+
+void Heros::changerCouleur(std::ostream& os, const int couleurAffichage) const
+{
+	os <<
+		couleurAffichage;
+	Personnage::changerCouleur(os, couleurAffichage);
+	os <<
+		couleurAffichage << "Ennemmi : " << obtenirNomEnnemi() << "\n";
+	// TODO: Pourquoi erreur sur << ?
+}
+
+void Heros::afficherAllies(ostream& os, int& couleur) const
+{
+	os <<
+		"Allies : ";
+	for (int i = 0; i < nomAllies_.size(); i++) 
+	{
+		os << "\t" << nomAllies_[i];
+	}
+	//os << CODE_COULEUR_FIN; // Verifier c'est quoi!
+}
+
+void Heros::mettreAllies(const vector<string> nomAllies)
+{
+	nomAllies_ = nomAllies;
+}
