3d0ff21c8d698e822c7742913e42245942e3af0b
Rodri Merino <rodrimerino201@gmail.com>
2022-11-07 16:50:44 -0500

    Terminer definition et implementation de base de VillainHeros.

--- Lab_4/VillainHeros.cpp
+++ Lab_4/VillainHeros.cpp
@@ -7,4 +7,30 @@
 #pragma once
 #include "VillainHeros.hpp"
 
-// TODO
+VillainHeros::VillainHeros(const Heros& hero, const Villain& villain) :
+	Heros(hero),
+	Villain(villain),
+	Personnage(villain.obtenirNom() + "-" + hero.obtenirNom(),
+		villain.obtenirParution() + "-" + hero.obtenirParution()),
+	mission_(villain.obtenirObjectif() + " dans le monde de " + hero.obtenirParution())
+{
+
+}
+
+void afficher(std::ostream& os) const
+{
+	changerCouleur(os, 50);
+}
+
+void changerCouleur(std::ostream& os, const int couleurAffichage) const
+{
+	os << couleurAffichage;
+	Villain::changerCouleur(os, couleurAffichage);
+	Heros::afficherAllies(os, couleurAffichage);
+	os << couleurAffichage;
+	os <<
+		"Mission spéciale : "
+		<< mission_;
+		// CODE COULEUR FIN
+}
+
--- Lab_4/VillainHeros.hpp
+++ Lab_4/VillainHeros.hpp
@@ -5,5 +5,18 @@
 //	Créé le 6 novembre 2022
 
 #pragma once
+#include <string>
+#include "Heros.hpp"
+#include "Villain.hpp"
 
-//TODO
+// TODO: Pourquoi cette erreur?
+class VillainHeros : public Heros, public Villain
+{
+public:
+	VillainHeros(const Heros& hero, const Villain& villain);
+	virtual void afficher(std::ostream& os) const override;
+	virtual void changerCouleur(std::ostream& os, const int couleurAffichage) const override;
+
+private:
+	string mission_;
+};
