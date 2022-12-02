134a2ae791e378ed011f3b96fc47412056d7bc4b
Rodri Merino <rodrimerino201@gmail.com>
2022-12-02 11:18:06 -0500

    Ajouter TODOs.

--- Lab_6/Article.hpp
+++ Lab_6/Article.hpp
@@ -13,15 +13,15 @@ struct Article
 	bool taxable;
 	double totalBeforeTaxes;
 
-	// Conteneur STL pour les articles
+	// TODO: Conteneur STL pour les articles
 	/*template<typename F>
 	std::shared_ptr<Concepteur> trouverConcepteur(const F fonction) {
 		return concepteurs.trouverElementQuelconque(fonction);
 	}*/
 
-	// fonction lambda pour total des taxes et algo STL -> parcourir articles dans vecteur SANS FOR OU WHILE
+	// TODO: fonction lambda pour total des taxes et algo STL -> parcourir articles dans vecteur SANS FOR OU WHILE
 
-	// Retirer article -> iterateur pour vector -> INDEX INTERDIT
+	// TODO: Retirer article -> iterateur pour vector -> INDEX INTERDIT
 };
 
 // TODO: lancer exception capturee par vue quand description vide ou prix = 0; Message different
